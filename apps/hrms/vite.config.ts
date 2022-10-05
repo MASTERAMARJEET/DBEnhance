98da109802a384e9d3b114c9cc2167f25e98ee93
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-10-05 18:32:42 +0530

    fix build settings

--- apps/hrms/vite.config.ts
+++ apps/hrms/vite.config.ts
@@ -1,15 +1,11 @@
 import solid from 'solid-start/vite'
 import vercel from 'solid-start-vercel'
-import node from 'solid-start-node'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     solid({
-      adapter: {
-        build: vercel({ edge: true }),
-        start: node(),
-      },
+      adapter: vercel({ edge: true }),
     }),
   ],
 })
--- apps/site/vite.config.ts
+++ apps/site/vite.config.ts
@@ -1,15 +1,12 @@
 import solid from 'solid-start/vite'
 import vercel from 'solid-start-vercel'
-import node from 'solid-start-node'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     solid({
-      adapter: {
-        build: vercel({ edge: true }),
-        start: node(),
-      },
+      ssr: true,
+      adapter: vercel({ edge: true }),
     }),
   ],
 })
