bf8a786a6ea1c1016661e04f65b8b6c3c2515387
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2023-01-22 13:00:48 +0530

    add tailwind config to each app

--- apps/hrms/postcss.config.cjs
+++ apps/hrms/postcss.config.cjs
@@ -1,9 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const config = require('tailwind-config/tailwind.config.cjs')
-
 module.exports = {
   plugins: {
-    tailwindcss: { config },
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
